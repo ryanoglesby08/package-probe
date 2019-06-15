c8b7a7c51d382f09d7e0e15b48d3d6a703391256
Ryan Oglesby <ryan.oglesby08@gmail.com>
2019-06-15 11:07:15 -0500

    feat: search in devDependencies in addition to dependencies

--- src/probe.js
+++ src/probe.js
@@ -1,30 +1,25 @@
 import isEmpty from 'lodash/isEmpty'
 
 import GithubApiClient from './githubApiClient'
+import { exactMatcher, partialMatcher } from './matchers'
 
 const fetchDependencyVersion = async (
   apiClient,
   searchTerm,
-  partialMatches,
+  collectMatches,
   repoFullName,
   url,
   path
 ) => {
   const packageJson = await apiClient.getContents(repoFullName, url, path)
-  const dependencies = packageJson.dependencies || {}
 
-  let data
-  if (partialMatches) {
-    data = Object.keys(dependencies)
-      .filter(dependency => dependency.includes(searchTerm))
-      .reduce(
-        (accumulator, dependency) =>
-          Object.assign({}, accumulator, { [dependency]: dependencies[dependency] }),
-        {}
-      )
-  }
+  const versionMatches = collectMatches(
+    packageJson.dependencies,
+    packageJson.devDependencies,
+    searchTerm
+  )
 
-  return partialMatches ? data : dependencies[searchTerm]
+  return versionMatches
 }
 
 const excludeRepos = (repositories, exclude) => {
@@ -37,13 +32,13 @@ const excludeRepos = (repositories, exclude) => {
   return repositories.filter(repository => !excludedRepos.includes(repository.repository.full_name))
 }
 
-const matchResults = async (apiClient, searchTerm, exclude, partialMatches, repositories) => {
+const matchResults = async (apiClient, searchTerm, collectMatches, exclude, repositories) => {
   const matchedRepos = await Promise.all(
     excludeRepos(repositories, exclude).map(async repository => {
       const version = await fetchDependencyVersion(
         apiClient,
         searchTerm,
-        partialMatches,
+        collectMatches,
         repository.repository.full_name,
         repository.url,
         repository.path
@@ -65,7 +60,10 @@ const enhanceWithLastEdit = async (apiClient, owner, matchedRepos) => {
       const repoInfo = await apiClient.getRepo(owner, repo.repositoryName)
       const lastEdit = repoInfo.headers['last-modified'] || ''
 
-      return Object.assign({}, repo, { lastEdit })
+      return {
+        ...repo,
+        lastEdit,
+      }
     })
   )
 }
@@ -80,12 +78,14 @@ const probe = async ({ accessToken, exclude, owner, partialMatches, searchTerm }
 
   const apiClient = new GithubApiClient(accessToken)
 
+  const collectMatches = partialMatches ? partialMatcher : exactMatcher
+
   const searchResults = await apiClient.searchCode(owner, searchTerm)
   const matchedRepos = await matchResults(
     apiClient,
     searchTerm,
+    collectMatches,
     exclude,
-    partialMatches,
     searchResults
   )
 
--- test/probe.spec.js
+++ test/probe.spec.js
@@ -33,19 +33,19 @@ it('finds versions of a package', async () => {
   })
 
   expect(results).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "lastEdit": "Sat, 06 Oct 2018 20:50:11 GMT",
-        "repositoryName": "my-cli",
-        "version": "^2.14.1",
-      },
-      Object {
-        "lastEdit": "Sat, 11 May 2019 16:13:38 GMT",
-        "repositoryName": "package-probe",
-        "version": "^2.9.0",
-      },
-    ]
-  `)
+                Array [
+                  Object {
+                    "lastEdit": "Sat, 06 Oct 2018 20:50:11 GMT",
+                    "repositoryName": "my-cli",
+                    "version": "^2.14.1",
+                  },
+                  Object {
+                    "lastEdit": "Sat, 11 May 2019 16:13:38 GMT",
+                    "repositoryName": "package-probe",
+                    "version": "^2.9.0",
+                  },
+                ]
+        `)
 })
 
 it('handles partial matches', async () => {
@@ -56,28 +56,30 @@ it('handles partial matches', async () => {
   })
 
   expect(results).toMatchInlineSnapshot(`
-        Array [
-          Object {
-            "lastEdit": "Sat, 04 May 2019 15:42:58 GMT",
-            "repositoryName": "the-eod-machine",
-            "version": Object {
-              "emotion": "^9.2.6",
-              "emotion-theming": "^9.2.6",
-              "react-emotion": "^9.2.6",
-            },
-          },
-          Object {
-            "lastEdit": "Sun, 02 Dec 2018 01:31:03 GMT",
-            "repositoryName": "ryanoglesby08.github.com",
-            "version": Object {
-              "emotion": "^9.1.2",
-              "emotion-server": "^9.1.2",
-              "gatsby-plugin-emotion": "^1.1.16",
-              "react-emotion": "^9.1.2",
-            },
-          },
-        ]
-    `)
+    Array [
+      Object {
+        "lastEdit": "Sat, 04 May 2019 15:42:58 GMT",
+        "repositoryName": "the-eod-machine",
+        "version": Object {
+          "emotion": "^9.2.6",
+          "emotion-theming": "^9.2.6",
+          "jest-emotion": "^9.2.7",
+          "react-emotion": "^9.2.6",
+        },
+      },
+      Object {
+        "lastEdit": "Sun, 02 Dec 2018 01:31:03 GMT",
+        "repositoryName": "ryanoglesby08.github.com",
+        "version": Object {
+          "babel-plugin-emotion": "^9.1.2",
+          "emotion": "^9.1.2",
+          "emotion-server": "^9.1.2",
+          "gatsby-plugin-emotion": "^1.1.16",
+          "react-emotion": "^9.1.2",
+        },
+      },
+    ]
+  `)
 })
 
 it('can exclude matches in specified repositories', async () => {
@@ -88,14 +90,14 @@ it('can exclude matches in specified repositories', async () => {
   })
 
   expect(results).toMatchInlineSnapshot(`
-                    Array [
-                      Object {
-                        "lastEdit": "Sun, 02 Dec 2018 01:31:03 GMT",
-                        "repositoryName": "ryanoglesby08.github.com",
-                        "version": "^4.17.5",
-                      },
-                    ]
-          `)
+                                Array [
+                                  Object {
+                                    "lastEdit": "Sun, 02 Dec 2018 01:31:03 GMT",
+                                    "repositoryName": "ryanoglesby08.github.com",
+                                    "version": "^4.17.5",
+                                  },
+                                ]
+                `)
 })
 
 it('uses an access token if provided', async () => {
@@ -112,19 +114,19 @@ it('uses an access token if provided', async () => {
   })
 
   expect(results).toMatchInlineSnapshot(`
-    Array [
-      Object {
-        "lastEdit": "Sat, 06 Oct 2018 20:50:11 GMT",
-        "repositoryName": "my-cli",
-        "version": "^2.14.1",
-      },
-      Object {
-        "lastEdit": "Sat, 11 May 2019 16:13:38 GMT",
-        "repositoryName": "package-probe",
-        "version": "^2.9.0",
-      },
-    ]
-  `)
+                Array [
+                  Object {
+                    "lastEdit": "Sat, 06 Oct 2018 20:50:11 GMT",
+                    "repositoryName": "my-cli",
+                    "version": "^2.14.1",
+                  },
+                  Object {
+                    "lastEdit": "Sat, 11 May 2019 16:13:38 GMT",
+                    "repositoryName": "package-probe",
+                    "version": "^2.9.0",
+                  },
+                ]
+        `)
 })
 
 it('requires a search term and owner', async () => {
@@ -139,4 +141,58 @@ it('requires a search term and owner', async () => {
       searchTerm: 'react',
     })
   ).rejects.toThrowError(/owner/)
+})
+
+it('finds matches in dev dependencies', async () => {
+  let results = await probe({
+    owner: 'ryanoglesby08',
+    searchTerm: 'angular',
+  })
+
+  expect(results).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "lastEdit": "Sun, 08 Oct 2017 17:31:47 GMT",
+                "repositoryName": "ng-inspect-watchers",
+                "version": "^1.4.3",
+              },
+            ]
+      `)
+
+  results = await probe({
+    owner: 'ryanoglesby08',
+    searchTerm: 'grunt',
+    partialMatches: true,
+  })
+
+  expect(results).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "lastEdit": "Sun, 08 Oct 2017 17:31:47 GMT",
+            "repositoryName": "ng-inspect-watchers",
+            "version": Object {
+              "grunt": "~0.4.5",
+              "grunt-contrib-compress": "~0.13.0",
+              "grunt-contrib-jshint": "~1.0",
+              "grunt-exec": "^0.4.6",
+            },
+          },
+          Object {
+            "lastEdit": "Sun, 02 Nov 2014 18:40:29 GMT",
+            "repositoryName": "L8-travis-build-monitor",
+            "version": Object {
+              "grunt": "0.4.1",
+              "grunt-compare-size": "0.4.0-rc.3",
+              "grunt-contrib-concat": "0.1.3",
+              "grunt-contrib-csslint": "0.1.1",
+              "grunt-contrib-cssmin": "0.4.2",
+              "grunt-contrib-jshint": "0.7.1",
+              "grunt-contrib-qunit": "0.2.0",
+              "grunt-contrib-uglify": "0.1.1",
+              "grunt-git-authors": "1.2.0",
+              "grunt-html": "0.3.3",
+            },
+          },
+        ]
+    `)
 })
